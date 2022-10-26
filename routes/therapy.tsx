14d1e3d693250c0485e1d47a800cfa7fd01afa85
Al Dee <amyalicedee@gmail.com>
2022-10-26 11:49:54 +0100

    Add testimonial html

--- routes/therapy.tsx
+++ routes/therapy.tsx
@@ -1,5 +1,20 @@
 import { Head } from "$fresh/runtime.ts";
 
+const testimonials = [
+  {
+    author: "Dave",
+    text: "Hello there",
+  },
+  {
+    author: "Sara",
+    text: "Hello you",
+  },
+  {
+    author: "Bilal",
+    text: "Hello mum",
+  },
+];
+
 export default function Home() {
   return (
     <>
@@ -9,6 +24,25 @@ export default function Home() {
       <div>
         <p>Ria Hope Therapy</p>
       </div>
+      <TestimonialsSection />
     </>
+  );
+}
+
+function TestimonialsSection() {
+  return (
+    <aside>
+      <h2>Client Reviews</h2>
+      <ul>
+        {testimonials.map((testimonial) => (
+          <li>
+            <figure>
+              <figcaption>{testimonial.author}</figcaption>
+              <blockquote>{testimonial.text}</blockquote>
+            </figure>
+          </li>
+        ))}
+      </ul>
+    </aside>
   );
 }
