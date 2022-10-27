c64020ea0f0193d321f7f48493a3d1b39fd3ede1
Al Dee <amyalicedee@gmail.com>
2022-10-27 17:11:52 +0100

    Add index header and styles

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,10 @@ export default function App(props: AppProps) {
       <Head>
         <meta name="description" content="Ria Hope Website" />
         <link rel="stylesheet" href={asset("/style.css")} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&family=Noto+Serif+Display:ital,wght@0,400;0,900;1,400;1,900&family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&family=Playfair+Display+SC&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <props.Component />
     </>
--- routes/index.tsx
+++ routes/index.tsx
@@ -14,6 +14,33 @@ export default function Home() {
           <a href="/therapy">Therapy</a>
         </p>
       </div>
+      <header>
+        <div class="title">
+          <h1>
+            <svg viewBox="0 0 158 26">
+              <text
+                x="6"
+                y="25"
+                textLength="100%"
+                lengthAdjust="spacingAndGlyphs"
+                style="
+              text-align: right; 
+              letter-spacing: 6px;
+          "
+              >
+                RIA HOPE
+              </text>
+            </svg>
+          </h1>
+          <h2>
+            Therapy, sexuality,<br></br>
+            intimacy, queer identity
+          </h2>
+        </div>
+        <div class="image">
+          <img src="ria-profile.jpg" alt="Ria Hope smiling" />
+        </div>
+      </header>
     </>
   );
 }
